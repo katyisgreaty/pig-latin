354854d91c49be5a8eca8f4eb89a1616df36b944
Katy Daviscourt and Tammy Dang <tammy@>
2017-01-18 11:26:42 -0800

    add javascript to pass first three specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
+var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+var integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 var pigLatin = function(word) {
-  if (isNaN(word)) {
-    alert("word yes");
 
+  var splitWords = word.toString().split("");
+  var number = false;
 
+  splitWords.forEach(function(element) {
+    var intquery = integers.indexOf(element);
+    // console.log(intquery);
+    if (intquery != -1) {
+      // console.log(word);
+      number = true;
+    }
+  })
 
-
-  } else {
-    return(word);
+  if (number) {
+    return word + " is not a word";
   }
+
+  return word + "ay";
+
+
+  // var vowel = false;
+  // splitWords.forEach(function(element) {
+  //   var vowquery = vowels.indexOf(element);
+  //   if (vowquery != -1) {
+  //     vowel = true;
+  //   }
+  // })
+  //
+  // if(vowel)
 };
 
 
@@ -16,9 +39,10 @@ $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
 
     event.preventDefault();
-    var word = parseInt($("input#word").val());
+    var word = $("input#word").val();
     var result = pigLatin(word);
-    $(".word").text(word);
+
+    $(".word").text(result);
       $("#result").show();
   });
 });
