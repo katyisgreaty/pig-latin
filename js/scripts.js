07c77c64806ca45efab8a8fcda7e6a22febe10f5
Katy Daviscourt and Tammy Dang <tammy@>
2017-01-18 15:14:56 -0800

    become stuck after first three specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
 var integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+var firstLetter = function(splitWords) {
+  var first = splitWords[0];
+  for(var i = 0; i < vowels.length; i += 1){
+    if (vowels[i] === first) {
+      return true;
+    }
+  }
+}
+
+
+var test = firstLetter(["b", "a", "b"]);
+console.log(test);
+
 var pigLatin = function(word) {
 
   var splitWords = word.toString().split("");
@@ -8,31 +21,40 @@ var pigLatin = function(word) {
 
   splitWords.forEach(function(element) {
     var intquery = integers.indexOf(element);
-    // console.log(intquery);
     if (intquery != -1) {
-      // console.log(word);
       number = true;
     }
-  })
+  });
 
   if (number) {
     return word + " is not a word";
   }
 
   return word + "ay";
+}
 
-
+  //
   // var vowel = false;
-  // splitWords.forEach(function(element) {
-  //   var vowquery = vowels.indexOf(element);
+
+
+  //
+  // for(var i = 0; i < 1; i++) {
+  //   var vowquery = vowels.indexOf(i=0);
   //   if (vowquery != -1) {
   //     vowel = true;
   //   }
   // })
-  //
-  // if(vowel)
-};
 
+var consonantMove = function(splitWords) {
+  splitWords.push(splitWords[0]);
+  splitWords.splice(0,1)
+  var newWord = splitWords.join("");
+  return newWord;
+}
+
+//   if(vowel)
+// };
+//
 
 //User Interface
 $(document).ready(function(){
